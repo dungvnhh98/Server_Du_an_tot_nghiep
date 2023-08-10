e452abef0d867b3ab9b525b4ee498b35683f84c6
NguyenDung <dungnmph18838@fpt.edu.vn>
2023-08-11 01:42:09 +0700

    11/8

--- models/suborder.js
+++ models/suborder.js
@@ -4,6 +4,7 @@ const subOrderSchema = new mongoose.Schema({
     idorder: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true },
     idproduct: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
     price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
 });
 
 const SubOrder = mongoose.model('SubOrder', subOrderSchema);
